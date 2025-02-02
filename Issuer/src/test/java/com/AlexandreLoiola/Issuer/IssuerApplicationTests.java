47f8ee555a0077dfa32b719b5a6defd1b9d7a549
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2025-02-02 20:49:25 -0300

    Refact: Change packages

---
+++ Issuer/src/test/java/com/AlexandreLoiola/Issuer/IssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlexandreLoiola.Issuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
